67f2d1f0f360335e56624eca2321a4f4599bdc25
Hany Hamed <h.hamed.elanwar@gmail.com>
2021-10-25 00:12:29 +0300

    [Pred-prey-2D] add OS flag

--- gym_predprey/envs/SelfPlayPredPrey1v1.py
+++ gym_predprey/envs/SelfPlayPredPrey1v1.py
@@ -32,9 +32,10 @@ import bach_utils.sorting as utsrt
 
 # TODO: Take care that with every reset we load a model, think about it
 
-OS = False
+OS = False#True
 
 # Parent class for all using SB3 functions to predict
+#TODO: Opponent_filename should be changed as it is not only for OS
 class SelfPlayEnvSB3:
     def __init__(self, log_dir,
                        algorithm_class, 
@@ -54,6 +55,7 @@ class SelfPlayEnvSB3:
         self._name = None
         self.archive = archive  # opponent archive
         self.OS = OS
+
         if(archive is None):
             self.OS = True
         self.states = None
@@ -63,6 +65,7 @@ class SelfPlayEnvSB3:
         self.target_opponent_policy_filename = policy_filename
 
     # TODO: This works fine for identical agents but different agents will not work as they won't have the same action spaec
+    # Solution: environment of Pred will have a modified step function that will call the parent environment step and then take what observation it is related to  
     # Compute actions for the opponent agent in the environment (Note: that the action for )
     # This is only be called for the opponent agent
     # TODO: This should be renamed -> compute_opponent_action or opponent_compute_policy-> Change them in PredPrey1v1.py
@@ -77,36 +80,6 @@ class SelfPlayEnvSB3:
                 # action, _ = self.opponent_policy.compute_action(obs) #it is predict because this is PPO from stable-baselines not rllib
 
             return action
-
-    # Not used
-    # def _sample_opponent(self):
-    #     print("Sample opponent")      
-    #     loading_path = os.path.join(self.log_dir, self.env_opponent_name)
-    #     files_list = utos.get_startswith(loading_path, self.startswith_keyword)
-    #     # print(f"Found {len(files_list)} files in {loading_path}")
-    #     opponent_filename = None
-    #     if(len(files_list) > 0):
-    #         if(self.opponent_selection == "random"):
-    #             opponent_filename = utos.get_random_from(files_list)[0]
-            
-    #         elif(self.opponent_selection == "latest"):
-    #             files_list = utsrt.sort_steps(files_list) # TODO: Take care about this computation bottelneck here O(NlogN), it will be a headach for large archives
-    #             latest = files_list[-1]
-    #             opponent_filename = latest
-
-    #         elif(self.opponent_selection == "highest"):
-    #             files_list = utsrt.sort_metric(files_list)
-    #             target = files_list[-1]
-    #             opponent_filename = target
-
-    #         elif(self.opponent_selection == "lowest"):
-    #             files_list = utsrt.sort_metric(files_list)
-    #             target = files_list[0]
-    #             opponent_filename = target
-
-    #         opponent_filename = os.path.join(self.log_dir, self.env_opponent_name, opponent_filename)
-    #         print(opponent_filename)
-    #         return opponent_filename
 
     def _load_opponent(self, opponent_filename):
         # print(f"Wants to load {opponent_filename}")
@@ -125,18 +98,11 @@ class SelfPlayEnvSB3:
 
     def reset(self):
         self.states = None
-        if(self.archive is None):
+        if(self.OS):
             print(f"Reset, env name: {self._name}, OS, target_policy: {self.target_opponent_policy_filename}")
-        else:
+        if(not self.OS):
             print(f"Reset, env name: {self._name}, archive_id: {self.archive.random_id}, target_policy: {self.target_opponent_policy_filename}")
         self._load_opponent(self.target_opponent_policy_filename)
-        # self.target_opponent_policy_filename = None
-        # if(self.target_opponent_policy_filename is not None):
-        #     self._load_opponent(self.target_opponent_policy_filename)
-        #     self.target_opponent_policy_filename = None # as if we want to have a specific opponent policy, we need to set it before each reset
-        # else:
-        #     opponent_filename = self._sample_opponent()
-        #     self._load_opponent(opponent_filename)
 
 class SelfPlayPredEnv(SelfPlayEnvSB3, PredPrey1v1Pred):
     # wrapper over the normal single player env, but loads the best self play model
@@ -166,65 +132,3 @@ class SelfPlayPreyEnv(SelfPlayEnvSB3, PredPrey1v1Prey):
     def reset(self):
         SelfPlayEnvSB3.reset(self)
         return PredPrey1v1Prey.reset(self)
-
-
-
-# import os
-# from gym_predprey.envs.PredPrey1v1 import PredPrey1v1Pred
-# from gym_predprey.envs.PredPrey1v1 import PredPrey1v1Prey
-
-# class SelfPlayEnvSB3:
-#     def __init__(self, **kwargs):
-#         self.log_dir = kwargs["log_dir"]
-#         self.algorithm_class = kwargs["algorithm_class"]
-#         self.best_model = None
-#         self.best_model_filename = None
-#         self.env_opponent_name = kwargs["env_opponent_name"]
-
-#     # TODO: This works fine for identical agents but different agents will not work as they won't have the same action spaec
-#     def compute_action(self, obs): # the policy
-#         if self.best_model is None:
-#             return self.action_space.sample() # return a random action
-#         else:
-#             action, _ = self.best_model.predict(obs) # it is predict because this is PPO from stable-baselines not rllib
-#             return action
-
-#     def reset(self):
-#         # load model if it's there
-#         modellist = [f for f in os.listdir(os.path.join(self.log_dir, self.env_opponent_name)) if f.startswith("history")]
-#         modellist.sort()
-#         if len(modellist) > 0:
-#             filename = os.path.join(self.log_dir, self.env_opponent_name, modellist[-1]) # the latest model
-#             if filename != self.best_model_filename:
-#                 print("loading model: ", filename)
-#                 self.best_model_filename = filename
-#                 if self.best_model is not None:
-#                     del self.best_model
-#                 self.best_model = self.algorithm_class.load(filename, env=self)
-
-# class SelfPlayPredEnv(SelfPlayEnvSB3, PredPrey1v1Pred):
-#     # wrapper over the normal single player env, but loads the best self play model
-#     def __init__(self, **kwargs):
-#         kwargs["env_opponent_name"] = "prey"
-#         SelfPlayEnvSB3.__init__(self, **kwargs)
-#         PredPrey1v1Pred.__init__(self)
-#         self.prey_policy = self # It replaces the policy for the other agent with the best policy that found during reset (This class have it)
-
-#     # Change to search only for the prey
-#     def reset(self):
-#         SelfPlayEnvSB3.reset(self)
-#         return PredPrey1v1Pred.reset(self)
-
-
-# class SelfPlayPreyEnv(SelfPlayEnvSB3, PredPrey1v1Prey):
-#     # wrapper over the normal single player env, but loads the best self play model
-#     def __init__(self, **kwargs):
-#         kwargs["env_opponent_name"] = "pred"
-#         SelfPlayEnvSB3.__init__(self, **kwargs)
-#         PredPrey1v1Prey.__init__(self)
-#         self.pred_policy = self # It replaces the policy for the other agent with the best policy that found during reset (This class have it)
-
-#     # Change to search only for the prey
-#     def reset(self):
-#         SelfPlayEnvSB3.reset(self)
-#         return PredPrey1v1Prey.reset(self)
