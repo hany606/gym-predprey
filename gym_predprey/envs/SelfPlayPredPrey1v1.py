3cacc24a87c56507cf3a9ef3b5af04f5dc60887b
Hany Hamed <h.hamed.elanwar@gmail.com>
2022-03-29 11:49:15 +0300

    Refactor the SelfPlay code

--- gym_predprey/envs/SelfPlayPredPrey1v1.py
+++ gym_predprey/envs/SelfPlayPredPrey1v1.py
@@ -1,12 +1,5 @@
-import os
 from gym_predprey.envs.PredPrey1v1 import PredPrey1v1Pred
 from gym_predprey.envs.PredPrey1v1 import PredPrey1v1Prey
-from stable_baselines3.ppo.ppo import PPO as sb3PPO
-
-import bach_utils.os as utos
-import bach_utils.list as utlst
-import bach_utils.sorting as utsrt
-import bach_utils.sampling as utsmpl
 
 # These envs are wrappers for the original environments to be able to train one agent while another agent in the environment is following a specific policy
 
@@ -32,99 +25,7 @@ import bach_utils.sampling as utsmpl
 #           and instead of passing the self.obs in PredPreyEvorobot._process_action "self.ac[2:] = self.prey_policy.compute_action(self.ob)", we will pass get_prey_obs(self.get_prey_obs())
 
 # TODO: Take care that with every reset we load a model, think about it
-
-OS = False#True
-
-# Parent class for all using SB3 functions to predict
-class SelfPlayEnvSB3:
-    def __init__(self, algorithm_class, archive, sample_after_reset, sampling_parameters):
-        self.algorithm_class = algorithm_class  # algorithm class for the opponent
-        self.opponent_policy = None             # The policy itself after it is loaded
-        self.opponent_policy_name = None    # Current loaded policy name -> File -> as it was implement first to be stored on disk (But now in cache=archive) 
-        self.target_opponent_policy_name = None
-        self._name = None
-        self.archive = archive  # opponent archive
-        self.OS = OS
-        self.sample_after_reset = sample_after_reset
-        self.sampling_parameters = sampling_parameters
-        self.reset_counter = 0
-
-        if(archive is None):
-            self.OS = True
-        self.states = None
-        
-
-    def set_target_opponent_policy_name(self, policy_name):
-        self.target_opponent_policy_name = policy_name
-
-    # TODO: This works fine for identical agents but different agents will not work as they won't have the same action spaec
-    # Solution: environment of Pred will have a modified step function that will call the parent environment step and then take what observation it is related to  
-    # Compute actions for the opponent agent in the environment (Note: that the action for )
-    # This is only be called for the opponent agent
-    # TODO: This should be renamed -> compute_opponent_action or opponent_compute_policy-> Change them in PredPrey1v1.py
-    def compute_action(self, obs): # the policy
-        if self.opponent_policy is None:
-            return self.action_space.sample() # return a random action
-        else:
-            action = None
-            if(isinstance(self.opponent_policy, sb3PPO)):
-                action, self.states = self.opponent_policy.predict(obs, state=self.states) #it is predict because this is PPO from stable-baselines not rllib
-            # if(isinstance(self.opponent_policy, rllibPPO)):
-                # action, _ = self.opponent_policy.compute_action(obs) #it is predict because this is PPO from stable-baselines not rllib
-
-            return action
-
-    def _load_opponent(self, opponent_name):
-        # print(f"Wants to load {opponent_name}")
-        # Prevent reloading the same policy again or reload empty policy -> empty policy means random policy
-        if opponent_name is not None:
-            if("Training" in self._name):
-                print(f"Add frequency +1 for {opponent_name}")
-                self.archive.add_freq(opponent_name, 1) 
-        
-            # To prevent the time for reloading it and it is already loaded
-            if(opponent_name != self.opponent_policy_name):
-                # print("loading model: ", opponent_name)
-                self.opponent_policy_name = opponent_name
-                if self.opponent_policy is not None:
-                    del self.opponent_policy
-                # if(isinstance(self.algorithm_class, sb3PPO) or isinstance(super(self.algorithm_class), sb3PPO)):
-                if(not self.OS):
-                    self.opponent_policy = self.archive.load(name=opponent_name, env=self, algorithm_class=self.algorithm_class) # here we load the opponent policy
-                if(self.OS):
-                    self.opponent_policy = self.algorithm_class.load(opponent_name, env=self) # here we load the opponent policy
-
-
-    def reset(self):
-        self.states = None
-        # if sample_after_reset flag is set then we need to sample from the archive here
-        if(self.sample_after_reset):
-            print("Sample after reset the environment")
-
-            opponent_selection = self.sampling_parameters["opponent_selection"]
-            sample_path = self.sampling_parameters["sample_path"]
-            startswith_keyword = "history"
-            randomly_reseed_sampling = self.sampling_parameters["randomly_reseed_sampling"]
-
-            sampled_opponent = None
-            if(not self.OS):
-                # print("Not OS")
-                archive = self.archive.get_sorted(opponent_selection) # this return [sorted_names, sorted_policies]
-                models_names = archive[0]
-                sampled_opponent = utsmpl.sample_opponents(models_names, 1, selection=opponent_selection, sorted=True, randomly_reseed=randomly_reseed_sampling, delta=self.archive.delta, idx=self.reset_counter)[0]
-            if(self.OS):
-                sampled_opponent = utsmpl.sample_opponents_os(sample_path, startswith_keyword, 1, selection=opponent_selection, randomly_reseed=randomly_reseed_sampling, delta=self.archive.delta, idx=self.reset_counter)[0]
-            self.target_opponent_policy_name = sampled_opponent
-        
-        if(self.OS):
-            print(f"Reset, env name: {self._name}, OS, target_policy: {self.target_opponent_policy_name}")
-        # if(not self.OS):
-        else:
-            print(f"Reset, env name: {self._name}, archive_id: {self.archive.random_id}, target_policy: {self.target_opponent_policy_name}")
-        
-        self._load_opponent(self.target_opponent_policy_name)
-        self.reset_counter += 1
-
+from bach_utils.SelfPlay import SelfPlayEnvSB3
 
 class SelfPlayPredEnv(SelfPlayEnvSB3, PredPrey1v1Pred):
     # wrapper over the normal single player env, but loads the best self play model
